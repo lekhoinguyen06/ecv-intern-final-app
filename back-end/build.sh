3628fbbbbb919680211a2a16ac46f8824789f2b8
lekhoinguyen <khoinguyenbenz@gmail.com>
2025-12-23 17:57:17 +0700

    feat:added health check route

--- back-end/build.sh
+++ back-end/build.sh
@@ -1,7 +1,20 @@
+pnpm run lint
 pnpm run build
 cd ../
+
+# Default tag message
+TAG_MESSAGE="deploy-$(date +%Y%m%d%H%M%S)"
+
+# Parse optional -m argument
+while getopts "m:" opt; do
+  case $opt in
+    m) TAG_MESSAGE="$OPTARG"; COMMIT_MESSAGE="$OPTARG" ;;
+    *) echo "Usage: $0 [-m \"commit/tag message\"]"; exit 1 ;;
+  esac
+done
+
 git add . 
-git commit -m "Build for ec2 deployment"
+git commit -m "$COMMIT_MESSAGE"
 git push
 cd ./back-end/
 
---
+++ back-end/run.sh
@@ -0,0 +1,5 @@
+cd ~/ecv-intern-final-app/
+git pull
+cd ./backend
+pnpm install
+pnpm run start:prod
