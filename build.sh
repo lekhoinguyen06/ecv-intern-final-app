843dfaab0220a0376b68c2bc2d746517f7ada4a6
lekhoinguyen <khoinguyenbenz@gmail.com>
2025-12-24 15:09:26 +0700

    Automated commit

--- build.sh
+++ build.sh
@@ -1,32 +1,51 @@
-# Process options
-while getopts "m:fp" opt; do
+while getopts "m:fpbah" opt; do
   case $opt in
-    m) COMMIT_MESSAGE="$OPTARG" ;;
-    f)
-      cd ./front-end
-      pnpm run build
-      cp -r ./out/* ../back-end/client/
-      cd ../
+    m) 
+      SHOULD_COMMIT=true
+      COMMIT_MESSAGE="$OPTARG"
       ;;
+    b) BUILD_BACKEND=true ;;
+    f) BUILD_FRONTEND=true ;;
     p) SHOULD_PUSH=true ;;
-    *) 
-      echo "Usage: $0" 
-      echo "[-m \"message\" to commit]";
-      echo "[-f to build frontend]";
-      echo "[-p to push to remote]";
-      exit 1 
+    a)
+      BUILD_BACKEND=true
+      BUILD_FRONTEND=true
+      SHOULD_COMMIT=true
+      SHOULD_PUSH=true
+      ;;
+    h)
+      echo "Usage: $0"
+      echo "[-m \"message\" to commit]"
+      echo "[-f to build frontend]"
+      echo "[-b to build backend]"
+      echo "[-p to push to remote]"
+      echo "[-a to do all (build frontend, build backend, commit, push)]"
+      echo "[-h to show this help]"
+      exit 0
+      ;;
+    *)
+      echo "Usage: $0 -h for help"
+      exit 1
       ;;
   esac
 done
 
-# Build server 
-cd ./back-end
-pnpm run lint
-pnpm run build
-cd ../
+if [ "$BUILD_FRONTEND" = true ]; then
+  cd ./front-end
+  pnpm run build
+  cp -r ./out/* ../back-end/client/
+  cd ../
+fi
 
-# Commit
-if [ -n "$COMMIT_MESSAGE" ]; then
+if [ "$BUILD_BACKEND" = true ]; then
+  cd ./back-end
+  pnpm run lint
+  pnpm run build
+  cd ../
+fi
+
+if [ "$SHOULD_COMMIT" = true ]; then
+  COMMIT_MESSAGE="${COMMIT_MESSAGE:-"Automated commit"}"
   git add .
   git commit -m "$COMMIT_MESSAGE"
 fi
@@ -34,4 +53,3 @@ fi
 if [ "$SHOULD_PUSH" = true ]; then
   git push
 fi
-
