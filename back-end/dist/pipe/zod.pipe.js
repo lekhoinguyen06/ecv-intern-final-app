6fc9df3e87fd5af4b524766f44a2749380862a71
lekhoinguyen <khoinguyenbenz@gmail.com>
2025-12-31 10:53:20 +0700

    fix: Zod pipe error message

--- back-end/dist/pipe/zod.pipe.js
+++ back-end/dist/pipe/zod.pipe.js
@@ -1,7 +1,41 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ZodValidationPipe = void 0;
 const common_1 = require("@nestjs/common");
+const z = __importStar(require("zod"));
 class ZodValidationPipe {
     schema;
     constructor(schema) {
@@ -12,8 +46,11 @@ class ZodValidationPipe {
             const parsedValue = this.schema.parse(value);
             return parsedValue;
         }
-        catch {
-            throw new common_1.BadRequestException('Validation failed');
+        catch (error) {
+            let message = '';
+            if (error instanceof z.ZodError)
+                message = error.message;
+            throw new common_1.BadRequestException('Validation failed:' + message);
         }
     }
 }
