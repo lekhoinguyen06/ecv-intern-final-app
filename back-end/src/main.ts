7f48e4f66fb34235e68de791f0855a88a8399c4e
TuanLeIsMe <leminhanhtuan8@gmail.com>
2025-12-31 14:56:18 +0700

    Add MetricInterceptor to global interceptors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,12 +4,18 @@ import { LogService } from './log/log.service';
 import { HTTPExceptionFilter } from './filter/http.filter';
 import { DatabaseExceptionFilter } from './filter/database.filter';
 import { SuccessResponseTransformInterceptor } from './intercept/response.intercept';
+import { MetricService } from './log/metric.service';
+import { MetricInterceptor } from './log/metric.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logService = app.get(LogService);
-  app.useGlobalInterceptors(new SuccessResponseTransformInterceptor());
+  const metricService = app.get(MetricService);
+  app.useGlobalInterceptors(
+    new SuccessResponseTransformInterceptor(),
+    new MetricInterceptor(MetricService),
+  );
   app.useGlobalFilters(
     new DatabaseExceptionFilter(logService),
     new HTTPExceptionFilter(logService),
