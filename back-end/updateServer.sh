9ddffdccd84001b585717b49ff6485b70d08e7ef
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-01-05 23:34:25 +0700

    ignore tests

--- back-end/updateServer.sh
+++ back-end/updateServer.sh
@@ -1,9 +1,9 @@
 cd ~/ecv-intern-final-app/
 git reset --hard
 git pull origin main
-bash ./build.sh -f -e
+bash ./build.sh -f -b
 cd ./back-end
 pnpm install
 pnpm run build
-# pnpm run test:e2e
+pnpm run test:e2e
 pnpm run start:prod
