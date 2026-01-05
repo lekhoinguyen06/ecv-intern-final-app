11bb4bdb11bc4c06d041fd21a63153737683e741
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-01-05 22:52:13 +0700

    fix: log service

--- back-end/src/app.service.ts
+++ back-end/src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
-import { LogService } from './log/log.service';
 
 @Injectable()
 export class AppService implements OnModuleDestroy {
-  constructor(private logService: LogService) {}
-
   onModuleDestroy() {
-    this.logService.silly('🛑 Application is shutting down');
+    console.log('🛑 Application is shutting down');
   }
 }
