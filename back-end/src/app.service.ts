9027083e403aa1cef3ee00f3278ec4dfba7b50c9
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-01-05 23:17:18 +0700

    fix: shutdown message

--- back-end/src/app.service.ts
+++ back-end/src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable, OnApplicationShutdown } from '@nestjs/common';
-import { LogService } from './log/log.service';
 
 @Injectable()
 export class AppService implements OnApplicationShutdown {
-  constructor(private logService: LogService) {}
-
   onApplicationShutdown() {
-    this.logService.silly('🛑 Application is shutting down');
+    console.log('🛑 Application is shutting down');
   }
 }
