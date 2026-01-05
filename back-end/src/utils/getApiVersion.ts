f75ce2f53a4ec3a3677970b784cb27b11b361fb7
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-01-05 13:39:32 +0700

    fix: metadata with apiVersion

--- back-end/src/utils/getApiVersion.ts
+++ back-end/src/utils/getApiVersion.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 export default function getApiVersion(): string {
   const packageJson = JSON.parse(
-    readFileSync(join(__dirname, '..', 'package.json'), 'utf-8'),
+    readFileSync(join(process.cwd(), 'package.json'), 'utf-8'),
   ) as { version: string };
   const apiVersion: string = process.env.API_VERSION ?? packageJson.version;
   return apiVersion;
